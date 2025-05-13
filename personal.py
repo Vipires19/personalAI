cf532887a278d78e2d49bada7875da3c1b2db11c
Vinícius Campos <163905335+Vipires19@users.noreply.github.com>
2025-05-13 09:48:52 -0300

    Update personal.py

--- personal.py
+++ personal.py
@@ -70,6 +70,7 @@ def show_student_dashboard():
     st.title("🏋️ Análise de Exercícios com IA")
     st.image("assets/logo.jpg", width=200)
     st.header(f"Bem-vindo, {st.session_state['name']}")
+    st.header("Dashboard Aluno")
 
     if authenticator.logout():
         st.session_state["authentication_status"] = None
@@ -162,6 +163,7 @@ def show_personal_dashboard():
     st.title("🏋️ Análise de Exercícios com IA")
     st.image("assets/logo.jpg", width=200)
     st.header(f"Bem-vindo, {st.session_state['name']}")
+    st.header('Dashboard Personal")
 
     if authenticator.logout():
         st.session_state["authentication_status"] = None
